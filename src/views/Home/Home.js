87ed334d189cd20188bfc373128f0134ae8c6f29
EmilyDSarani <emily.sarani@gmail.com>
2021-11-29 16:34:32 -0600

    OHHHHHHH

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -7,32 +7,27 @@ import './Home.css'
 
 export default function Home() {
   // add useState calls here for title, subtitle, font, align, and text
-  const [title, setTitle] = useState('')
-  const [subtitle, setSubTitle] = useState('')
-  const [font, setFont] = useState('')
-  const [align, setAlign] = useState('')
-  const [text, setText] = useState('')
+  const [title, setTitle] = useState()
+  const [subtitle, setSubtitle] = useState()
+  const [font, setFont] = useState()
+  const [align, setAlign] = useState()
+  const [text, setText] = useState()
 
   return (
     <main>
       {/* pass the state variables as props to the presentational components */}
-      <Preview
-        title={title}
-        onTitleChange={setTitle}
-        subtitle={subtitle}
-        onSubtitleChange={setSubTitle}
-      />
+      <Preview title={title} subtitle={subtitle} font={font} align={align} text={text} />
       <Editor
         title={title}
-        onTitleChange={setTitle}
+        setTitle={setTitle}
         subtitle={subtitle}
-        onSubtitleChange={setSubTitle}
+        setSubtitle={setSubtitle}
         font={font}
-        onFontChange={setFont}
+        setFont={setFont}
         align={align}
-        onAlignChange={setAlign}
+        setAlign={setAlign}
         text={text}
-        onTextChange={setText}
+        setText={setText}
       />
     </main>
   )
